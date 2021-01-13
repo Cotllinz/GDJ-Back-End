495447a748f8a3e0a384793ef3fcb203f780ab77
Mohammad Ilham Nurdhi Hafsyah <ilhamhafsyah15@gmail.com>
2021-01-13 17:56:46 +0700

    cek konflik

--- app.js
+++ app.js
@@ -1,6 +1,1 @@
-const express = require('express')
-const morgan = require('morgan')
-const bodyParser = require('body-parser')
-const routeNav = require('./src/routesNavigation.js')
-const cors = require('cors')
-require('dotenv').config()
+// CEK KONFLIK
