77ef7aa5bc3e210c22897fa7d6d0e3f16e10c4cf
Hasbi Alwi Kusmana <67422750+hasbiak@users.noreply.github.com>
2021-01-15 19:25:18 +0700

    add multer profile company and patch company profile

--- src/routeNavigation.js
+++ src/routeNavigation.js
@@ -1,6 +1,10 @@
 const router = require('express').Router()
 const user = require('./routes/user')
 const hire = require('./routes/hire')
+const company = require('./routes/companyProfile')
+
 router.use('/hire', hire)
 router.use('/user', user)
+router.use('/company', company)
+
 module.exports = router
