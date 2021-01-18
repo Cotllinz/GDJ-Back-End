3cb7768edf85cb97bf528f232d83b5e58d1001ae
Mohammad Ilham Nurdhi Hafsyah <ilhamhafsyah15@gmail.com>
2021-01-18 21:49:13 +0700

    update redis hire routes

--- src/routes/hire.js
+++ src/routes/hire.js
@@ -1,11 +1,11 @@
 const router = require('express').Router()
-// const { authorization, isSeeker, isRecruiter } = require('../middleware/auth')
+const { authorization, isSeeker, isRecruiter } = require('../middleware/auth')
 const uploadFiles = require('../middleware/multerFileHire')
 
 const { hire, notif } = require('../controller/hire')
 const { notifRedis, clearRedis } = require('../middleware/redis')
 
-router.post('/', clearRedis, uploadFiles, hire)
-router.get('/notif/:id', notifRedis, notif)
+router.post('/', authorization, isRecruiter, clearRedis, uploadFiles, hire)
+router.get('/notif/:id', authorization, isSeeker, notifRedis, notif)
 
 module.exports = router
