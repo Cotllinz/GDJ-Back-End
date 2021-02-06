092fc003636041868e3bae3d802274b3dd596c5d
andrawan <andrawan.ep@gmail.com>
2021-02-07 05:33:57 +0700

    Backend Chat DONE

---
+++ src/model/chat.js
@@ -0,0 +1,102 @@
+const connection = require('../config/mysql')
+
+module.exports = {
+  createRoomModel: (setData) => {
+    return new Promise((resolve, reject) => {
+      connection.query(
+        'INSERT INTO roomchat SET ?',
+        setData,
+        (error, result) => {
+          !error ? resolve(result) : reject(new Error(error))
+        }
+      )
+    })
+  },
+  checkRoomModel: (a, b) => {
+    return new Promise((resolve, reject) => {
+      connection.query(
+        'SELECT * FROM roomchat WHERE sender = ? AND receiver = ?',
+        [a, b],
+        (error, result) => {
+          if (!error) {
+            resolve(result)
+          } else {
+            console.log(error)
+            reject(new Error(error))
+          }
+        }
+      )
+    })
+  },
+  getRoomModel: (id_user) => {
+    return new Promise((resolve, reject) => {
+      connection.query(
+        'SELECT roomIdUniq, user_account.id_user,  user_account.username FROM roomchat RIGHT JOIN user_account ON user_account.id_user = receiver WHERE sender = ? ',
+        [id_user],
+        (error, result) => {
+          if (!error) {
+            resolve(result)
+          } else {
+            console.log(error)
+            reject(new Error(error))
+          }
+        }
+      )
+    })
+  },
+  getRoom2UserModel: (sender, receiver) => {
+    return new Promise((resolve, reject) => {
+      connection.query(
+        'SELECT * from roomchat WHERE sender = ? AND receiver = ? ',
+        [sender, receiver],
+        (error, result) => {
+          if (!error) {
+            resolve(result)
+          } else {
+            console.log(error)
+            reject(new Error(error))
+          }
+        }
+      )
+    })
+  },
+  sendMessageModel: (setData) => {
+    return new Promise((resolve, reject) => {
+      connection.query('INSERT INTO chat SET ?', setData, (error, result) => {
+        !error ? resolve((result = setData)) : reject(new Error(error))
+      })
+    })
+  },
+  getMessageModel: (id) => {
+    return new Promise((resolve, reject) => {
+      connection.query(
+        'SELECT roomIdUniq, user_account.id_user, user_account.username , chat.createdAt, message  FROM chat RIGHT JOIN user_account ON user_account.id_user = sender WHERE roomIdUniq = ?',
+        [id],
+        (error, result) => {
+          if (!error) {
+            resolve(result)
+          } else {
+            console.log(error)
+            reject(new Error(error))
+          }
+        }
+      )
+    })
+  },
+  getLastMessageModel: (a) => {
+    return new Promise((resolve, reject) => {
+      connection.query(
+        'SELECT message, createdAt FROM chat WHERE roomIdUniq = ? ORDER BY createdAt DESC LIMIT 1',
+        [a],
+        (error, result) => {
+          if (!error) {
+            resolve(result)
+          } else {
+            console.log(error)
+            reject(new Error(error))
+          }
+        }
+      )
+    })
+  }
+}
