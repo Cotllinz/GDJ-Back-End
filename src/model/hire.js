d8909970ca10e16d5541801e40df007b254ddbc5
Cotllinz <63383858+Cotllinz@users.noreply.github.com>
2021-02-08 01:01:21 +0800

    Update DESC

--- src/model/hire.js
+++ src/model/hire.js
@@ -24,7 +24,7 @@ module.exports = {
     return new Promise((resolve, reject) => {
       connection.query(
         // `SELECT id_recruiter, jobs_needed, desc_jobs FROM hired_jobs WHERE id_pekerja = ${id}`,
-        `SELECT image_recruiter, company_name, jobs_needed, hired_jobs.created_at FROM user_account JOIN hired_jobs ON id_user = id_recruiter JOIN profile_recruiter ON hired_jobs.id_recruiter = profile_recruiter.id_recruiter WHERE id_pekerja = ${id} && roles = 1`,
+        `SELECT image_recruiter, company_name, jobs_needed, hired_jobs.created_at FROM user_account JOIN hired_jobs ON id_user = id_recruiter JOIN profile_recruiter ON hired_jobs.id_recruiter = profile_recruiter.id_recruiter WHERE id_pekerja = ${id} && roles = 1 order by  hired_jobs.created_at DESC`,
         // `SELECT hired_jobs.jobs_needed, hired_jobs.desc_jobs, profile_recruiter.city_recruiter, profile_recruiter.image_recruiter FROM hired_jobs JOIN profile_recruiter ON hired_jobs.id_recruiter = profile_recruiter.id_recruiter = ${id}`,
         (error, result) => {
           !error ? resolve(result) : reject(new Error(error))
